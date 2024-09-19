7a4195c7febe4dc5e74b668769dfc5344ab4130e
ADMIN <Vaishali12bhati@gmail.com>
2024-09-20 04:03:10 +0530

    example

--- src/test/java/com/thetestingacademy/APITEST002.java
+++ src/test/java/com/thetestingacademy/APITEST002.java
@@ -1,7 +1,22 @@
 package com.thetestingacademy;
 
+import io.restassured.RestAssured;
+
 public class APITEST002 {
     public static void main(String[] args) {
-        System.out.println("welecome to Api PROJECT ");
+        System.out.println("START THE API AUTOMATION ");
+        // WE USEF GHERKINS SYNTAX
+        //given() : url , headers , body , or payload
+        // when () : http methods : get ,post, put , patch,delete
+        //then () : verify  the response er == ar
+        //example below
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("/booking/2").log().all()
+                .when()
+                 .get()
+                .then().log().all()
+                .statusCode(200);
     }
 }
