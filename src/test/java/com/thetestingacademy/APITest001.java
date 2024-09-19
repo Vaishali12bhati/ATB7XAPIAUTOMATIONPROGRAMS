1b8c40c3f11093faf5f6761aa126656f80669c6a
ADMIN <Vaishali12bhati@gmail.com>
2024-09-20 03:37:01 +0530

    Initial commit

---
+++ src/test/java/com/thetestingacademy/APITest001.java
@@ -0,0 +1,12 @@
+package com.thetestingacademy;
+
+import io.restassured.RestAssured;
+
+public class APITest001 {
+    public static void main(String[] args) {
+
+
+        //restassure
+        System.out.println("hello world");
+    }
+}
